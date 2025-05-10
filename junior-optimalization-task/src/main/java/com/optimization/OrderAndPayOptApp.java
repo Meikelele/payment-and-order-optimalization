76f1877aa3d6d99ebc8c545b6a06d6f1e38c632d
Meikelele <michal.bak2002@interia.pl>
2025-05-10 12:35:31 +0200

    initial commit

---
+++ junior-optimalization-task/src/main/java/com/optimization/OrderAndPayOptApp.java
@@ -0,0 +1,13 @@
+package com.optimization;
+
+/**
+ * Hello world!
+ *
+ */
+public class OrderAndPayOptApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
