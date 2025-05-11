31fb6e91c8ac25a1c36b56c0e60267fe160b6891
Meikelele <michal.bak2002@interia.pl>
2025-05-11 13:57:45 +0200

    wrote some test for validate arguments - OrderAndPayOptTest

--- junior-optimalization-task/src/test/java/com/optimization/OrderAndPayOptTest.java
+++ junior-optimalization-task/src/test/java/com/optimization/OrderAndPayOptTest.java
@@ -1,4 +1,41 @@
 package com.optimization;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class OrderAndPayOptTest {
+
+    @Test
+    void validateArguemnts_zeroArgumentShouldThrowException() {
+        // ACT
+        String[] args = {};
+
+        // ARRANGE
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> OrderAndPayOptApp.validateArguments(args));
+
+        // ASSERT
+        assertTrue(ex.getMessage().contains("Please give two params: <orders.json> <paymentmethods.json>"));
+    }
+
+    @Test
+    void validateArguemnts_oneArgumentShouldThrowException() {
+        // ACT
+        String[] args = {"orders.json"};
+
+        // ARRANGE
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> OrderAndPayOptApp.validateArguments(args));
+
+        // ASSERT
+        assertTrue(ex.getMessage().contains("Please give two params: <orders.json> <paymentmethods.json>"));
+    }
+
+    @Test
+    void validateArguemnts_twoArgumentShouldNotThrowException() {
+        // ACT
+        String[] args = {"orders.json", "paymentmethods.json"};
+
+        // ARRANGE & ASSERT
+        assertDoesNotThrow(() -> OrderAndPayOptApp.validateArguments(args));
+    }
 }
