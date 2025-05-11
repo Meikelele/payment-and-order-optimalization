a1baa40414e8ca147ce45bbc59d3b9b2aa40fcae
Meikelele <michal.bak2002@interia.pl>
2025-05-11 13:32:48 +0200

    wrote tests for Allocation class

--- junior-optimalization-task/src/test/java/com/optimization/AllocationTest.java
+++ junior-optimalization-task/src/test/java/com/optimization/AllocationTest.java
@@ -1,4 +1,61 @@
 package com.optimization;
 
+import com.optimization.model.Allocation;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class AllocationTest {
+
+    @Test
+    void gettersShouldReturnContructorDataFullPointsPayment() {
+        // ARRANGE
+        String expectedMethodId = "PUNKTY";
+        String expectedCardId = null;
+        double expectedPointsUsage = 69.0;
+        double expectedCardUsage = 0.0;
+        double expectedDiscount = 15.0;
+
+        Allocation testAllocation = new Allocation(expectedMethodId, expectedCardId, expectedPointsUsage, expectedCardUsage, expectedDiscount);
+
+        // ACT
+        String actualMethodId = testAllocation.getMethodId();
+        String actualCardId = testAllocation.getCardId();
+        double actualPointsUsage = testAllocation.getPointsUsage();
+        double actualCardUsage = testAllocation.getCardUsage();
+        double actualDiscount = testAllocation.getDiscount();
+
+        // ASSERT
+        assertEquals(expectedMethodId, actualMethodId);
+        assertNull(actualCardId);
+        assertEquals(expectedPointsUsage, actualPointsUsage);
+        assertEquals(expectedCardUsage, actualCardUsage);
+        assertEquals(expectedDiscount, actualDiscount);
+    }
+
+    @Test
+    void constructorShouldSetMethodIdAsCardIdInCaseOfFullCardPayment() {
+        // ARRANGE
+        String expectedMethodId = "mZysk";
+        double expectedPointsUsage = 0.0;
+        double expectedCardUsage = 180.0;
+        double expectedDiscount = 18.0;
+
+        // ACT
+        Allocation testAllocation = new Allocation(expectedMethodId, expectedPointsUsage, expectedCardUsage, expectedDiscount);
+
+        // ASSERT
+        assertEquals(expectedMethodId, testAllocation.getMethodId());
+        assertEquals(expectedMethodId, testAllocation.getCardId());
+        assertEquals(expectedPointsUsage, testAllocation.getPointsUsage());
+        assertEquals(expectedCardUsage, testAllocation.getCardUsage());
+        assertEquals(expectedDiscount, testAllocation.getDiscount());
+    }
+
+
+
+
+
+
+
+
+
 }
